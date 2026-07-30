7c4b0421379b0bdd835ed2399df8e6af0fcd38b9
Vivtyk <vivtyk0@gmail.com>
2026-07-30 18:05:48 +0300

    Delete Задачі з програмування Вівтюк Георгій directory

--- Задачі з програмування Вівтюк Георгій/585.py
+++
@@ -1,6 +0,0 @@
-text = input().split()
-max_len = 0
-for word in text:
-    if len(word) > max_len:
-        max_len = len(word)
-print(max_len)
--- Задачі з програмування Вівтюк Георгій/698.py
+++
@@ -1,7 +0,0 @@
-import itertools
-
-def get_permutations(sequence):
-    return [list(p) for p in itertools.permutations(sequence)]
-
-data = [int(x) for x in input().split()]
-print(get_permutations(data))
